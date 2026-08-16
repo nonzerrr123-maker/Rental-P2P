2083646c38f9c9b6fa58de88d36740334bfd73ab
Suntisuk promwang <nonzerrr123@gmail.com>
2026-08-16 23:50:28 +0700

    feat: expose urgent availability controls

--- my-app/app/lend/page.tsx
+++ my-app/app/lend/page.tsx
@@ -1,7 +1,13 @@
 import { requireVerifiedUserPage } from "@/lib/auth/authorization";
 import LendForm from "./lend-form";
+import UrgentListingManager from "./urgent-listing-manager";
 
 export default async function LendPage() {
   await requireVerifiedUserPage("/lend");
-  return <LendForm />;
+  return (
+    <>
+      <LendForm />
+      <UrgentListingManager />
+    </>
+  );
 }
